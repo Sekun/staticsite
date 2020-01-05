502f166ed7fe0ac59ef2a386aabc4681bb64e994
Vincent <vincent.spies@hers.be>
2020-01-05 16:02:59 +0100

    Add requirements

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,9 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+STATIC_PATHS = ['images', 'admin']
+TEMPLATE_PAGES = {"admin/index.html": "admin/index.html"}
+
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
